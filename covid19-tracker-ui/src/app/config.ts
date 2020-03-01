55b50530c87c37ae53317a6d97872cafc5e8f716
anuragroy17 <anuragroy17@gmail.com>
2020-03-01 16:52:21 +0530

    Update config.ts

--- covid19-tracker-ui/src/app/config.ts
+++ covid19-tracker-ui/src/app/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-    // apiUrl: 'http://localhost:8080'
-    apiUrl: 'https://covid19-tracker-app.herokuapp.com'
+    apiUrl: 'http://localhost:8080'
+    // apiUrl: 'https://covid19-tracker-app.herokuapp.com'
   };

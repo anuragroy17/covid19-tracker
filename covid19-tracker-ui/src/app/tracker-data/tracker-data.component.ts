28768e7f6c8ced701755998cb78cb7230f8abdf0
anuragroy17 <anuragroy17@gmail.com>
2020-03-01 13:57:33 +0530

    fixing viewport

--- covid19-tracker-ui/src/app/tracker-data/tracker-data.component.ts
+++ covid19-tracker-ui/src/app/tracker-data/tracker-data.component.ts
@@ -1,27 +1,46 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { CovidDataService, LocationStats } from '../_shared';
-import { DatatableComponent } from '@swimlane/ngx-datatable';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { CovidDataService, LocationStats } from "../_shared";
+import { DatatableComponent } from "@swimlane/ngx-datatable";
 
 @Component({
-  selector: 'app-tracker-data',
-  templateUrl: './tracker-data.component.html',
-  styleUrls: ['./tracker-data.component.scss']
+  selector: "app-tracker-data",
+  templateUrl: "./tracker-data.component.html",
+  styleUrls: ["./tracker-data.component.scss"]
 })
-export class TrackerDataComponent implements OnInit {  
+export class TrackerDataComponent implements OnInit {
 
+  screenHeight: number;
+  screenWidth: number;
+  lastColName: String = "Difference from Previous Day";
+  secLastCol: string = "Latest Total Cases";
+  showDetail: boolean = false;  
   loadingIndicator: boolean = false;
+
   locationStats: LocationStats[] = [];
   columns = [
-    { prop: 'state' },
-    { name: 'Country' },
-    { name: 'Latest Total Cases' },
-    { prop: 'diffFromPrevDay', name: 'Difference from Previous Day'}
+    { prop: "state" },
+    { name: "Country" },
+    { name: "Latest Total Cases" },
+    { prop: "diffFromPrevDay", name: "Difference from Previous Day" }
   ];
   filteredData = [];
 
   @ViewChild(DatatableComponent, { static: false }) table: DatatableComponent;
 
-  constructor(private covidDataService:CovidDataService) { }
+  constructor(private covidDataService: CovidDataService) {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+    // console.log(this.screenWidth)
+    if(this.screenWidth < 1187){
+      this.lastColName = "DFPD";
+      this.secLastCol = "LTC"
+      this.showDetail = true;
+    }else{
+      this.lastColName = "Difference from Previous Day";
+      this.secLastCol = "Latest Total Cases";
+      this.showDetail = false;
+    }
+  }
 
   ngOnInit(): void {
     this.loadingIndicator = true;
@@ -30,31 +49,36 @@ export class TrackerDataComponent implements OnInit {
       this.filteredData = [...locationStats];
       this.loadingIndicator = false;
       // console.log(this.locationStats)
-  });
+    });
   }
 
   // filters results
-filterDatatable(event){
-  // get the value of the key pressed and make it lowercase
-  let val = event.target.value.toLowerCase();
-  // get the amount of columns in the table
-  let colsAmt = this.columns.length;
-  // get the key names of each column in the dataset
-  let keys = Object.keys(this.filteredData[0]);
-  // assign filtered matches to the active datatable
-  this.locationStats = this.filteredData.filter(function(item){
-    // iterate through each row's column data
-    for (let i=0; i<colsAmt; i++){
-      // check for a match
-      if (item[keys[i]].toString().toLowerCase().indexOf(val) !== -1 || !val){
-        // found match, return true to add to result set
-        return true;
+  filterDatatable(event) {
+    // get the value of the key pressed and make it lowercase
+    let val = event.target.value.toLowerCase();
+    // get the amount of columns in the table
+    let colsAmt = this.columns.length;
+    // get the key names of each column in the dataset
+    let keys = Object.keys(this.filteredData[0]);
+    // assign filtered matches to the active datatable
+    this.locationStats = this.filteredData.filter(function(item) {
+      // iterate through each row's column data
+      for (let i = 0; i < colsAmt; i++) {
+        // check for a match
+        if (
+          item[keys[i]]
+            .toString()
+            .toLowerCase()
+            .indexOf(val) !== -1 ||
+          !val
+        ) {
+          // found match, return true to add to result set
+          return true;
+        }
       }
-    }
-  });
-  // whenever the filter changes, always go back to the first page
-  this.table.offset = 0;
-}
-
+    });
+    // whenever the filter changes, always go back to the first page
+    this.table.offset = 0;
+  }
 
 }
